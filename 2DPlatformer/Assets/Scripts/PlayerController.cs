262ca8ee839741166cf777d25eb258d919630c91
Xiomaraze <allie.arnott@gmail.com>
2024-11-29 15:01:48 -0500

    Set up basic Movement (Task1 Week 10)

--- 2DPlatformer/Assets/Scripts/PlayerController.cs
+++ 2DPlatformer/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,24 +10,52 @@ public class PlayerController : MonoBehaviour
         left, right
     }
 
+    Rigidbody2D rb2;
+    BoxCollider2D playerCollider;
+    Vector2 playerInput = Vector2.zero; //im specifically moving the player input variable from inside fixedupdate to here to keep consistent track of the last input the player used. sorry keely
+    public float Speed;
+    float gravityDrag = 4f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2 = gameObject.GetComponent<Rigidbody2D>();
+        playerCollider = gameObject.GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         //The input from the player needs to be determined and then passed in the to the MovementUpdate which should
         //manage the actual movement of the character.
-        Vector2 playerInput = new Vector2();
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerInput = new Vector2(0, playerInput.y);
+            playerInput = playerInput + Vector2.left;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            playerInput = new Vector2(0, playerInput.y);
+            playerInput = playerInput + Vector2.right;
+        }
+        else
+        {
+            playerInput = new Vector2(0, playerInput.y);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(playerInput);
+        }
+
         MovementUpdate(playerInput);
     }
 
-    private void MovementUpdate(Vector2 playerInput)
+    private void MovementUpdate(Vector2 moveInput)
     {
 
+        rb2.AddForce(moveInput * Speed * gravityDrag);
     }
 
     public bool IsWalking()
